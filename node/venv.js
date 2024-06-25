4ee8527f1a0688c89d3b05cccc2622c4c4324967
Alexandre Alapetite <alexandre@alapetite.fr>
2024-06-25 15:51:49 +0200

    spawn instead of exec

--- node/venv.js
+++ node/venv.js
@@ -10,22 +10,41 @@ module.exports = function(RED) {
         const json = fs.readFileSync(jsonPath)
         const pythonPath = JSON.parse(json).NODE_PYENV_PYTHON
 
-        const execSync = require('child_process').execSync
-        let code = ""
+        const child_process = require('child_process')
 
         node.on('input', function(msg) {
-            const message = Buffer.from(JSON.stringify(msg)).toString('base64')
-            const command = `${pythonPath} -c "import base64;import json;msg=json.loads(base64.b64decode(r'${message}'));exec(open(r'${filePath}').read())"`
-
-            if(config.code !== null && config.code !== "") {
+            let code = ''
+            if(config.code !== null && config.code !== '') {
                 code = config.code
             } else {
                 code = msg.code
             }
             fs.writeFileSync(filePath, code)
             
-            msg.payload = String(execSync(command))
-            node.send(msg)
+            const message = Buffer.from(JSON.stringify(msg)).toString('base64')
+            const args = ['-c', `import base64;import json;msg=json.loads(base64.b64decode(r'${message}'));exec(open(r'${filePath}').read())`]
+            const pythonProcess = child_process.spawn(pythonPath, args)
+            let stdoutData = ''
+            let stderrData = ''
+
+            pythonProcess.on('message', console.log)
+
+            pythonProcess.stdout.on('data', (chunk) => {
+                stdoutData += chunk.toString()
+            })
+            
+            pythonProcess.stderr.on('data', (chunk) => {
+                stderrData += chunk.toString()
+            })
+            
+            pythonProcess.on('close', (exitCode) => {
+                if (exitCode !== 0) {
+                    node.error(stderrData)
+                } else {
+                    msg.payload = stdoutData
+                    node.send(msg)
+                }
+            })
         })
     }
     RED.nodes.registerType("venv", Venv)
