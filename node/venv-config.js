1a6639ed144f29cb3fa1064e8e2047d569d8fdcc
Frederik Palludan Madsen <frederik.palludan@alexandra.dk>
2024-07-03 08:39:39 +0200

    Reformat with prettier

--- node/venv-config.js
+++ node/venv-config.js
@@ -1,27 +1,31 @@
-module.exports = function(RED) {
-    function venvConfig(config) {
-        RED.nodes.createNode(this, config)
-        this.venvname = config.venvname
-        this.version = config.version
+module.exports = function (RED) {
+  function venvConfig(config) {
+    RED.nodes.createNode(this, config)
+    this.venvname = config.venvname
+    this.version = config.version
 
-        const path = require('path')
-        const fs = require('fs')
-        const execSync = require('child_process').execSync
-        const setupPath = path.join(path.dirname(__dirname), 'setup.py')
-        let venvPath = path.join(path.dirname(__dirname), this.venvname)
+    const path = require('path')
+    const fs = require('fs')
+    const execSync = require('child_process').execSync
+    const setupPath = path.join(path.dirname(__dirname), 'setup.py')
+    let venvPath = path.join(path.dirname(__dirname), this.venvname)
 
-        let command = `python ${setupPath} "${this.venvname}"`
-        if(typeof this.version !== 'undefined' && this.version !== '' && this.version !== 'default') {
-            command += ` ${this.version}`
-        }
-        execSync(command)
-
-        this.on('close', function(removed, done) {
-            if (removed) {
-                fs.rmSync(venvPath, { recursive: true, force: true })
-            }
-            done()
-        })
+    let command = `python ${setupPath} "${this.venvname}"`
+    if (
+      typeof this.version !== 'undefined' &&
+      this.version !== '' &&
+      this.version !== 'default'
+    ) {
+      command += ` ${this.version}`
     }
-    RED.nodes.registerType("venv-config",venvConfig)
+    execSync(command)
+
+    this.on('close', function (removed, done) {
+      if (removed) {
+        fs.rmSync(venvPath, { recursive: true, force: true })
+      }
+      done()
+    })
+  }
+  RED.nodes.registerType('venv-config', venvConfig)
 }
--- node/venv.js
+++ node/venv.js
@@ -1,78 +1,96 @@
-module.exports = function(RED) {
-    function Venv(config) {
-        RED.nodes.createNode(this,config)
-        const node = this
-        this.venvconfig = RED.nodes.getNode(config.venvconfig)
+module.exports = function (RED) {
+  function Venv(config) {
+    RED.nodes.createNode(this, config)
+    const node = this
+    this.venvconfig = RED.nodes.getNode(config.venvconfig)
 
-        if(!this.venvconfig) {
-            node.send({ payload:"Missing virtual environment configuration"})
-            return
-        }
-
-        const fs = require('fs')
-        const path = require('path')
-        const child_process = require('child_process')
-
-        node.status({fill:"green", shape:"dot", text:"Standby"})
-        let runningScripts = 0
-
-        let jsonPath = path.join(path.dirname(__dirname), this.venvconfig.venvname, 'path.json')
-        let filePath = path.join(path.dirname(__dirname), this.venvconfig.venvname, this.id + '.py')
-        if(path.isAbsolute(this.venvconfig.venvname)) {
-            jsonPath = path.join(this.venvconfig.venvname, 'path.json')
-            filePath = path.join(this.venvconfig.venvname, this.id + '.py')
-        }
-        const json = fs.readFileSync(jsonPath)
-        const pythonPath = JSON.parse(json).NODE_PYENV_PYTHON
-
-        node.on('input', function(msg) {
-            runningScripts++
-            node.status({fill:"blue", shape:"dot", text: `Script instances running: ${runningScripts}`})
-            
-            let code = ''
-            if(typeof config.code !== 'undefined' && config.code !== '') {
-                code = config.code
-            } else {
-                code = msg.code ?? ''
-            }
-            fs.writeFileSync(filePath, code)
-
-            const message = Buffer.from(JSON.stringify(msg)).toString('base64')
-            const args = ['-c', `import base64;import json;msg=json.loads(base64.b64decode(r'${message}'));exec(open(r'${filePath}').read())`]
-            const pythonProcess = child_process.spawn(pythonPath, args)
-            let stdoutData = ''
-            let stderrData = ''
-
-            pythonProcess.on('message', console.log)
-
-            pythonProcess.stdout.on('data', (chunk) => {
-                stdoutData += chunk.toString()
-            })
-
-            pythonProcess.stderr.on('data', (chunk) => {
-                stderrData += chunk.toString()
-            })
-
-            pythonProcess.on('close', (exitCode) => {
-                runningScripts--
-                if (exitCode !== 0) {
-                    node.status({fill:"red", shape:"dot", text:"Error"})
-                    node.error(`Error ${exitCode}: ` + stderrData)
-                } else {
-                    msg.payload = stdoutData
-                    node.send(msg)
-                    if(runningScripts === 0) {
-                        node.status({fill:"green", shape:"dot", text:"Standby"})
-                    }
-                    else {
-                        node.status({fill:"blue", shape:"dot", text: `Script instances running: ${runningScripts}`})
-                    }
-                }
-
-                stdoutData = ''
-                stderrData = ''
-            })
-        })
+    if (!this.venvconfig) {
+      node.send({ payload: 'Missing virtual environment configuration' })
+      return
     }
-    RED.nodes.registerType("venv", Venv)
+
+    const fs = require('fs')
+    const path = require('path')
+    const child_process = require('child_process')
+
+    node.status({ fill: 'green', shape: 'dot', text: 'Standby' })
+    let runningScripts = 0
+
+    let jsonPath = path.join(
+      path.dirname(__dirname),
+      this.venvconfig.venvname,
+      'path.json'
+    )
+    let filePath = path.join(
+      path.dirname(__dirname),
+      this.venvconfig.venvname,
+      this.id + '.py'
+    )
+    if (path.isAbsolute(this.venvconfig.venvname)) {
+      jsonPath = path.join(this.venvconfig.venvname, 'path.json')
+      filePath = path.join(this.venvconfig.venvname, this.id + '.py')
+    }
+    const json = fs.readFileSync(jsonPath)
+    const pythonPath = JSON.parse(json).NODE_PYENV_PYTHON
+
+    node.on('input', function (msg) {
+      runningScripts++
+      node.status({
+        fill: 'blue',
+        shape: 'dot',
+        text: `Script instances running: ${runningScripts}`,
+      })
+
+      let code = ''
+      if (typeof config.code !== 'undefined' && config.code !== '') {
+        code = config.code
+      } else {
+        code = msg.code ?? ''
+      }
+      fs.writeFileSync(filePath, code)
+
+      const message = Buffer.from(JSON.stringify(msg)).toString('base64')
+      const args = [
+        '-c',
+        `import base64;import json;msg=json.loads(base64.b64decode(r'${message}'));exec(open(r'${filePath}').read())`,
+      ]
+      const pythonProcess = child_process.spawn(pythonPath, args)
+      let stdoutData = ''
+      let stderrData = ''
+
+      pythonProcess.on('message', console.log)
+
+      pythonProcess.stdout.on('data', chunk => {
+        stdoutData += chunk.toString()
+      })
+
+      pythonProcess.stderr.on('data', chunk => {
+        stderrData += chunk.toString()
+      })
+
+      pythonProcess.on('close', exitCode => {
+        runningScripts--
+        if (exitCode !== 0) {
+          node.status({ fill: 'red', shape: 'dot', text: 'Error' })
+          node.error(`Error ${exitCode}: ` + stderrData)
+        } else {
+          msg.payload = stdoutData
+          node.send(msg)
+          if (runningScripts === 0) {
+            node.status({ fill: 'green', shape: 'dot', text: 'Standby' })
+          } else {
+            node.status({
+              fill: 'blue',
+              shape: 'dot',
+              text: `Script instances running: ${runningScripts}`,
+            })
+          }
+        }
+
+        stdoutData = ''
+        stderrData = ''
+      })
+    })
+  }
+  RED.nodes.registerType('venv', Venv)
 }
